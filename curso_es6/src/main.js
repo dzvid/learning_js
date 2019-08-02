4bb8541bb38cf489d265eff2662b0ab161f40953
tukno <oliveiradavid007@gmail.com>
2019-08-02 14:31:34 -0400

    App template was built

--- curso_es6/src/main.js
+++ curso_es6/src/main.js
@@ -456,3 +456,76 @@
 // // };
 
 // // buscaUsuario('diego3g');
+class App{
+    constructor(){
+        this.repositories = [];
+
+        this.formEl = document.getElementById('repo-form');
+        this.listEl = document.getElementById('repo-list');
+
+        this.registerHandlers();
+    }
+
+    // Register the events of the app
+    registerHandlers(){
+        this.formEl.onsubmit = event => this.addRepository(event);
+
+    }
+
+    // Add a repository to the repositories list
+    addRepository(){
+        event.preventDefault();
+
+        this.repositories.push({
+            name: 'rocketseat.com.br',
+            description: 'Tire a sua ideia do papel e dê vida à sua startup.',
+            avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
+            html_url: 'https://github.com/rocketseat'
+        });
+
+        this.render();
+    }
+
+    // Clear the list of repositories and render the list again
+    render(){
+        this.clearList();
+    }
+
+    // Clear the content of the element list repositories 
+    clearList(){
+        this.listEl.innerHTML = '';
+
+        //create and render the element on screen
+        this.repositories.forEach(
+            repo => {
+                //create elements
+                let imgEl = document.createElement('img');
+                imgEl.setAttribute('src', repo.avatar_url);
+
+                let titleEl = document.createElement('strong');
+                titleEl.appendChild(document.createTextNode(repo.name));
+
+                let descriptionEl = document.createElement('p');
+                descriptionEl.appendChild(document.createTextNode(repo.description));
+
+                let linkEl = document.createElement('a');
+                linkEl.setAttribute('target', '_blank');
+                linkEl.setAttribute('href', repo.html_url);
+                linkEl.appendChild(document.createTextNode('Acessar'));
+
+                let listItemEl = document.createElement('li');
+
+                //render elements
+                listItemEl.appendChild(imgEl);
+                listItemEl.appendChild(titleEl);
+                listItemEl.appendChild(descriptionEl);
+                listItemEl.appendChild(linkEl);
+
+                    //add item in the list  
+                this.listEl.appendChild(listItemEl);
+            }
+        );
+    }
+}
+
+new App();
